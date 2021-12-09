949223fd3b65598d03e1c30f0ef50e88c5b0a20b
Kenneth Jensen <42805394+poochy442@users.noreply.github.com>
2021-12-09 15:30:24 +0100

    Timing fix

--- src/Components/Review/ReviewDetails.jsx
+++ src/Components/Review/ReviewDetails.jsx
@@ -1,5 +1,4 @@
 import { useSelector } from 'react-redux';
-import { connect } from 'react-redux';
 import { useFirestoreConnect } from 'react-redux-firebase';
 import '../../Styles/Review/ReviewDetails.scss'
 
@@ -7,10 +6,10 @@ const ReviewDetails = (props) => {
 	const {review} = props;
 	useFirestoreConnect([{
 		collection: 'users',
-		doc: review.reviewerId
-	}]);
-	const user = useSelector((state) => state.firestore.data.users)
-	const reviewer = user[review.reviewerId]
+		doc: review.reviewerId,
+		storeAs: 'reviewer'
+	}])
+	const reviewer = useSelector((state) => state.firestore.data.reviewer);
 
 	const scoreDisplay = (
 		<div className='scoreDisplay'>
@@ -26,20 +25,14 @@ const ReviewDetails = (props) => {
 			<p className='reviewContent'>{review.review}</p>
 			{scoreDisplay}
 			<div className='bottomContainer'>
-				<div className='reviewerContainer'>
+				{reviewer ? <div className='reviewerContainer'>
 					<div className='avatar'>{reviewer.initials}</div>
 					<p className='userName'>{reviewer.username}</p>
-				</div>
+				</div> : <div>Loading reviewer...</div>}
 				<p className='reviewDate'>{new Date(review.createdAt.seconds * 1000).toLocaleString()}</p>
 			</div>
 		</div>
 	)
 }
 
-const mapStateToProps = (state) => {
-	return {
-		profile: state.firestore
-	}
-}
-
-export default connect(mapStateToProps)(ReviewDetails)
+export default ReviewDetails
